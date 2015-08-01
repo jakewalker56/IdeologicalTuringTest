9fc11d2bf6b3a79229192ac04c3f5b37406f32a0
Jacob Walker <jake@Jacobs-MacBook-Pro.local>
2015-08-01 02:04:48 -0500

    showing only my votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,7 +6,7 @@ class VotesController < ApplicationController
   # GET /votes
   # GET /votes.json
   def index
-    @votes = Vote.all
+    @votes = Vote.all.where(user_id: current_user.id)
   end
 
   # GET /votes/1
